79be0c6e0f972277f409ebb0732c75a5755a7ce2
TarKhachonsak <khachonsak117@gmail.com>
2024-11-29 14:08:56 +0700

    Create index.ts

---
+++ app/constants/index.ts
@@ -0,0 +1,21 @@
+import project1 from "../assets/project/project1.jpg";
+
+export const HERO_CONTENT = `I am Software Engineering student passionate about enhancing skills in Frontend Development. Proficient in building web interfaces using Next.js and Tailwind CSS, with strong teamwork abilities to deliver high-quality projects.`;
+
+export const PROJECTS = [
+  {
+    title: "Food Blog Website",
+    image: project1,
+    description:
+      "A food blog website built using ReactJS and Tailwind CSS, featuring dynamic content loading, user authentication, and search functionality.",
+    technologies: ["React Framework", "Tailwind"],
+  },
+];
+
+export const CONTACT = [
+    {
+      address: "888 AAAA, AAAA, 57123",
+      phoneNo: "999-9999-999",
+      email: "khachonsak117@gmail.com",
+    },
+]
